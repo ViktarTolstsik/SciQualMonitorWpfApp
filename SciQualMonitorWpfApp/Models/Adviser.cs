2fda254389228d67889a400721f0ea6fa9060ecc
Viktar Tolstsik <viktor.tolstik.96@gmail.com>
2024-04-05 09:09:18 +0300

    Adviser model added

---
+++ SciQualMonitorWpfApp/Models/Adviser.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SciQualMonitorWpfApp.Models
+{
+    public class Adviser
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Patronym { get; set; }
+        public string AcademicDegree {  get; set; }
+        public string Specialty { get; set; }
+    }
+}
