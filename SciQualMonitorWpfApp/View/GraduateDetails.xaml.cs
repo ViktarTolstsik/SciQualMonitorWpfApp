bec8e5ac94b197717419d183a24022d6866cd0e9
Viktar Tolstsik <viktor.tolstik.96@gmail.com>
2024-04-18 12:55:55 +0300

    Details page definition started

---
+++ SciQualMonitorWpfApp/View/GraduateDetails.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace SciQualMonitorWpfApp.View
+{
+    /// <summary>
+    /// Логика взаимодействия для GraduateDetails.xaml
+    /// </summary>
+    public partial class GraduateDetails : Window
+    {
+        public GraduateDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
