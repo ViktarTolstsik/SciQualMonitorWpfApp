ee5cae29a873a760460f076b50c282a6641f201a
Viktar Tolstsik <viktor.tolstik.96@gmail.com>
2024-04-22 10:24:47 +0300

    Modal window appearance definition started

--- SciQualMonitorWpfApp/View/MainWindow.xaml.cs
+++ SciQualMonitorWpfApp/View/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
-﻿using SciQualMonitorWpfApp.Helpers.Data;
+﻿using Microsoft.EntityFrameworkCore.Migrations.Operations;
+using SciQualMonitorWpfApp.Helpers.Data;
 using SciQualMonitorWpfApp.Models;
+using SciQualMonitorWpfApp.View;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +15,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -25,6 +29,7 @@ namespace SciQualMonitorWpfApp
     {
         public ObservableCollection<Graduate> graduates;
         public ObservableCollection<Adviser> advisers;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -34,12 +39,22 @@ namespace SciQualMonitorWpfApp
             graduates = new ObservableCollection<Graduate>(GraduatesBaseData<Graduate>.GetGraduates());
             advisers = new ObservableCollection<Adviser>(GraduatesBaseData<Adviser>.GetAdvisers());
 
-            foreach (var graduate in graduates)
+
+            UpdateView();
+
+        }
+
+        public void UpdateView()
+        {
+            var viewGraduates = new ObservableCollection<Graduate>(GraduatesBaseData<Graduate>.GetGraduates());
+            foreach (var graduate in viewGraduates)
             {
                 Adviser? adviser = advisers.FirstOrDefault(x => x.Id == graduate.SciAdviserId);
                 graduate.Adviser = adviser;
             }
-            dataGrid.ItemsSource = graduates;
+            dataGrid.ItemsSource = viewGraduates;
+
+            titlePlaceholder.Text = $"Количество записей: {graduates.Count}";
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -67,6 +82,27 @@ namespace SciQualMonitorWpfApp
         private void btnMinimize_Click(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
+        }
+
+        private void viewButton_Click(object sender, RoutedEventArgs e)
+        {
+            GraduateDetails detailsWindow = new GraduateDetails();
+            detailsWindow.Owner = this;
+            this.Opacity = 0.7;
+            detailsWindow.ShowDialog();
+
+            this.Opacity = 1;
+            UpdateView();
+        }
+
+        private void editButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void refreshButton_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateView();
         }
     }
 }
