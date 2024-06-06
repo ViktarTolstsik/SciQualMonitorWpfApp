a5e626cc16571ca38746b641d3d60aaba45a5af4
Viktar Tolstsik <viktor.tolstik.96@gmail.com>
2024-06-06 12:20:21 +0300

    New Record window generated content defined

--- SciQualMonitorWpfApp/View/AddRecordWindow.xaml.cs
+++ SciQualMonitorWpfApp/View/AddRecordWindow.xaml.cs
@@ -31,6 +31,7 @@ namespace SciQualMonitorWpfApp.View
             advisers = new ObservableCollection<Adviser>(GraduatesBaseData<Adviser>.GetAdvisers());
             advisersComboBox.ItemsSource = advisers;
             NewRecordId = lastRecordId + 1;
+            contentFill();
         }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
@@ -55,6 +56,42 @@ namespace SciQualMonitorWpfApp.View
             advisers = new ObservableCollection<Adviser>(GraduatesBaseData<Adviser>.GetAdvisers());
             advisersComboBox.ItemsSource = advisers;
 
+        }
+
+        private void contentFill()
+        {
+            int thisYear = DateTime.UtcNow.Year;
+            for (int i = thisYear - 100; i <= thisYear; i++)
+            {
+                birthYearComboBox.Items.Add(i.ToString());
+                expulsionYearComboBox.Items.Add(i.ToString());
+                higherEdGradYearComboBox.Items.Add(i.ToString());
+                interruptionYearComboBox.Items.Add(i.ToString());
+                mastersGradYearComboBox.Items.Add(i.ToString());
+                restorationYearComboBox.Items.Add(i.ToString());
+                BIPAdmissionYearComboBox.Items.Add(i.ToString());
+                BIPContractYearComboBox.Items.Add(i.ToString());
+            }
+            for (int i = thisYear - 30; i < thisYear + 10; i++)
+            {
+                BIPGraduationYearComboBox.Items.Add(i.ToString());
+                BIPThesisDefYearComboBox.Items.Add(i.ToString());
+            }
+            for (int i = 0; i < 100; i++)
+            {
+                actCountComboBox.Items.Add(i.ToString());
+                monoCountComboBox.Items.Add(i.ToString());
+                thesesCountYear1ComboBox.Items.Add(i.ToString());
+                thesesCountYear2ComboBox.Items.Add(i.ToString());
+                thesesCountYear3ComboBox.Items.Add(i.ToString());
+                thesesCountYear4ComboBox.Items.Add(i.ToString());
+                thesesCountYear5ComboBox.Items.Add(i.ToString());
+                articlesCountYear1ComboBox.Items.Add(i.ToString());
+                articlesCountYear2ComboBox.Items.Add(i.ToString());
+                articlesCountYear3ComboBox.Items.Add(i.ToString());
+                articlesCountYear4ComboBox.Items.Add(i.ToString());
+                articlesCountYear5ComboBox.Items.Add(i.ToString());
+            }
         }
     }
 }
